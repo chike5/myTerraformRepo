9cfb25c22dcbc14db3f0118e8b0ff765f60dc4cc
chike5 <choikeluu@yahoo.com>
2023-02-01 13:10:58 -0600

    Added terraform files

---
+++ sonar.tf
@@ -0,0 +1,57 @@
+resource "aws_vpc" "sonar" {
+  cidr_block = "172.16.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "sonar_vpc"
+  }
+}
+
+ resource "aws_security_group" "security_sonar_group_2023" {
+      name        = "security_sonar_group_2023"
+      description = "security group for Sonar"
+      ingress {
+        from_port   = 9000
+        to_port     = 9000
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+
+     ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+
+     # outbound from Sonar server
+      egress {
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+
+      tags= {
+        Name = "security_sonar"
+      }
+    }
+
+    resource "aws_instance" "mySonarInstance" {
+      ami           = "ami-08fdec01f5df9998f"
+      key_name = "MyJenkinsEC2key"
+      instance_type = "t2.micro"
+      vpc_security_group_ids = [aws_security_group.security_sonar_group_2023.id]
+
+      tags= {
+        Name = "sonar_instance"
+      }
+    }
+
+# Create Elastic IP address for Sonar instance
+resource "aws_eip" "mySonarInstance" {
+  vpc      = true
+  instance = aws_instance.mySonarInstance.id
+tags= {
+    Name = "sonar_elastic_ip"
+  }
+}
